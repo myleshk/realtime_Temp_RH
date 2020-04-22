730c3d6bbacd73ba7748997d7f4bdb09bf4bfb05
Myles FANG <i@myles.hk>
2020-04-22 11:17:51 +0800

    Updates
    - add latest data table
    - fix graph
    - add AC state line
    - abort production build

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,28 @@
 var gulp = require('gulp'),
     browserify = require('browserify'),
     source = require('vinyl-source-stream'),
-    uglify = require('gulp-uglify'),
+    // uglify = require('gulp-uglify'),
     streamify = require('gulp-streamify'),
-    watchify = require('watchify');
+    watchify = require('watchify'),
+    browserSync = require('browser-sync');
 
+gulp.task('default', ['compile_dev', 'serve']);
+
+gulp.task('serve', [], function () {
+    // .init starts the server
+    const bs = browserSync.create();
+    bs.init({
+        server: "./",
+        port: 3010
+    });
+});
 
 gulp.task('compile_dev', function () {
     // place code for your default task here
     var b = browserify({
         entries: ['./src/main.js'],
         plugin: [watchify]
-    }).transform('browserify-css', {global: true});
+    }).transform('browserify-css', { global: true });
 
     function bundle() {
         return b.bundle()
@@ -22,15 +33,4 @@ gulp.task('compile_dev', function () {
     b.on('update', bundle);
 
     return bundle();
-});
-
-
-gulp.task('compile_prod', function () {
-    // place code for your default task here
-    return browserify('./src/main.js')
-        .transform('browserify-css', {global: true})
-        .bundle()
-        .pipe(source('bundle.js'))
-        .pipe(streamify(uglify()))
-        .pipe(gulp.dest('./build/'));
 });
